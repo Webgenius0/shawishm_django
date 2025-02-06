18d805230970462aee8c70df1fea967fa792aa3d
snrredoy <snrredoy3@example.com>
2025-02-06 10:23:54 +0600

    add

--- projects/settings.py
+++ projects/settings.py
@@ -81,14 +81,7 @@ WSGI_APPLICATION = 'projects.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shawishm',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+
 }
 
 
